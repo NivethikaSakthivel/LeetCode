b2bb36c80cf4e7f2fc361a359e0518eb5fa55dac
Nivethika S <117746313+NivethikaSakthivel@users.noreply.github.com>
2024-10-04 14:21:38 +0530

    Time: 18 ms (24.18%), Space: 57.6 MB (63.48%) - LeetHub

---
+++ 2581-divide-players-into-teams-of-equal-skill/2581-divide-players-into-teams-of-equal-skill.java
@@ -0,0 +1,15 @@
+class Solution {
+    public long dividePlayers(int[] skill) {
+        Arrays.sort(skill);
+        long res=0,temp=(long)skill[0]+(long)skill[skill.length-1],sum=0;
+        for(int i=0;i<skill.length;i++){
+            long prod=(long)skill[i]*(long)skill[skill.length-i-1];
+            sum=(long)skill[i]+(long)skill[skill.length-i-1];
+            if(temp!=sum){
+                return -1;
+            }
+            res+=prod;
+        }
+        return res/2;
+    }
+}
