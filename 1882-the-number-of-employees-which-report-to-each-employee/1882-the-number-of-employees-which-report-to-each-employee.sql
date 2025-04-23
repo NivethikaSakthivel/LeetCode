ea0ffa62e54fd3c05bd1fb9ae5ac9ee93343cb5e
Nivethika S <117746313+NivethikaSakthivel@users.noreply.github.com>
2025-04-23 15:55:02 +0530

    Time: 901 ms (50.03%), Space: 0B (100%) - LeetHub

--- 1882-the-number-of-employees-which-report-to-each-employee/1882-the-number-of-employees-which-report-to-each-employee.sql
+++ 1882-the-number-of-employees-which-report-to-each-employee/1882-the-number-of-employees-which-report-to-each-employee.sql
@@ -3,5 +3,5 @@ select e1.employee_id as employee_id, e1.name as name, count(e2.reports_to) as r
 from Employees e1
 join Employees e2 on e1.employee_id=e2.reports_to
 group by e1.employee_id
-having count(e2.reports_to)>0
+
 order by e1.employee_id;
