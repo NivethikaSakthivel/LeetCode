b9d5ddcc603ba3ed4e231604a26642eec96c5e01
Nivethika S <117746313+NivethikaSakthivel@users.noreply.github.com>
2025-07-20 19:49:51 +0530

    Time: 2 ms (73.71%), Space: 53.3 MB (28.88%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,14 +1,11 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        Map<Integer,Integer> freq=new HashMap<>();
-        int max=0, maxnum=nums[0];
+        int count=0;
+        int candidate=0;
         for(int num:nums){
-            freq.put(num, freq.getOrDefault(num,0)+1);
-            if(freq.get(num)>max){
-                max=freq.get(num);
-                maxnum=num;
-            }
+            if(count==0) candidate=num;
+            count+=(num==candidate)?1:-1;
         }
-        return maxnum;
+        return candidate;
     }
 }
