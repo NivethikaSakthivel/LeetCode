37aab9b875f87fab9eb3570d0670d016f5605cc2
Nivethika S <117746313+NivethikaSakthivel@users.noreply.github.com>
2025-08-02 09:29:31 +0530

    Time: 1266 ms (5.03%), Space: 0B (100%) - LeetHub

---
+++ 1523-capital-gainloss/1523-capital-gainloss.sql
@@ -0,0 +1,4 @@
+SELECT stock_name, 
+    sum(case when operation='sell' then price else 0 end)-sum(case when operation='buy' then price else 0 end) as capital_gain_loss
+FROM Stocks 
+group by stock_name;
