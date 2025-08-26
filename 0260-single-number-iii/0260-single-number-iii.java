35ade4af2bd01b350127a74fb2c5a164dd405a48
Nivethika S <117746313+NivethikaSakthivel@users.noreply.github.com>
2025-08-26 14:33:46 +0530

    Time: 1 ms (99.74%), Space: 45.8 MB (66.79%) - LeetHub

---
+++ 0260-single-number-iii/0260-single-number-iii.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] singleNumber(int[] nums) {
+        int xor=0;
+        for(int num:nums){
+            xor^=num;
+        }
+        int diff=xor& -xor;
+        int x=0, y=0;
+        for(int num:nums){
+            if((num&diff)==0){
+                x^=num;
+            }
+            else{
+                y^=num;
+            }
+        }
+        return new int[]{x,y};
+    }
+}
