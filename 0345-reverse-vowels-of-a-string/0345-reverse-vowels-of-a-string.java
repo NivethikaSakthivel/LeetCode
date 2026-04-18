2a32559b1fbb5215b3787ebf8246e67b44071352
Nivethika S <117746313+NivethikaSakthivel@users.noreply.github.com>
2026-04-18 10:53:09 +0530

    Time: 2 ms (99.4%), Space: 46.3 MB (92.6%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
@@ -1,17 +1,19 @@
 class Solution {
     public String reverseVowels(String s) {
-        String vow="aeiouAEIOU";
-        char[] ch=s.toCharArray();
+        char[] arr=s.toCharArray();
         int left=0, right=s.length()-1;
         while(left<right){
-            while(left<right && vow.indexOf(ch[left])==-1) left++;
-            while(left<right && vow.indexOf(ch[right])==-1) right--;
-            char temp=ch[left];
-            ch[left]=ch[right];
-            ch[right]=temp;
+            while(left<right && !isvowel(arr[left])) left++;
+            while(left<right && !isvowel(arr[right])) right--;
+            char temp=arr[left];
+            arr[left]=arr[right];
+            arr[right]=temp;
             left++;
             right--;
         }
-        return new String(ch);
+        return new String(arr);
+    }
+    private boolean isvowel(char c){
+        return c=='a' || c=='e' || c=='i' || c=='o' || c=='u' || c=='A' || c=='E' || c=='I' || c=='O' || c=='U';
     }
 }
